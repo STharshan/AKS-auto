6567d3d5d9b2eecdf3ac47db7cd48bb952395ff9
STharshan <sivatharshan81@gmail.com>
2025-10-03 10:00:33 +0530

    a

--- src/components/Home/ServiceSection.jsx
+++ src/components/Home/ServiceSection.jsx
@@ -23,30 +23,6 @@ const services = [
     image: "/carmechanic.avif",
     path: "/services/mot",
   },
-  // {
-  //   title: "Vehicle Wrapping",
-  //   desc: "We provide wheel alignment with the added benifit of improved vehicle perfomance and tire longvity",
-  //   image: "/paint.webp",
-  //   path: "/services/wheel-alignment",
-  // },
-  // {
-  //   title: "WINDOW TINT",
-  //   desc: "We offer proffessional window tinting for a sleek look and enhanced comfort",
-  //   image: "/bule.avif",
-  //   path: "/services/window-tint",
-  // },
-  // {
-  //   title: "Specialist Coatings",
-  //   desc: "Experts in timing chains, timing belts and one of the few wet timing chain specialist",
-  //   image: "/shine.webp",
-  //   path: "/services/belts-chains",
-  // },
-  // {
-  //   title: "Graphics",
-  //   desc: "We offer the laest dealer-level diagnostics",
-  //   image: "/homepaint.avif",
-  //   path: "/services/diagnostics",
-  // },
 ];
 
 const ServiceSection = () => {
@@ -72,20 +48,50 @@ const ServiceSection = () => {
 
   return (
     <section className="py-25 px-4 bg-white dark:bg-black relative transition-colors duration-300">
-      <div className="max-w-7xl mb-20 mx-auto relative">
+       <div className="absolute left-0 w-full z-10  border-gray-700" data-aos="fade-up" data-aos-delay="700">
+        <div className="mx-auto backdrop-blur-sm flex flex-col sm:flex-row items-center justify-center px-6 py-3 text-white text-sm font-semibold">
+          {/* Rating Section */}
+          <div className="flex items-center gap-3 dark:bg-black border border-gray-600 px-4 py-2 rounded-full order-1 sm:order-2 mb-3 sm:mb-0">
+            {/* Stars */}
+            <div className="flex text-yellow-400 text-lg">
+              <span>★</span>
+              <span>★</span>
+              <span>★</span>
+              <span>★</span>
+              <span>★</span>
+            </div>
+            {/* Rating Text */}
+            <span className=" text-sm font-medium text-black dark:text-white">4.7/5 from</span>
+            {/* Reviews Badge */}
+            <span className="bg-secondary border text-black dark:text-white border-gray-600 px-3 py-1 rounded-full text-xs font-medium">
+              84+ Reviews
+            </span>
+          </div>
+
+          {/* TRC Logo */}
+          {/* <div className="flex items-center order-2 sm:order-1">
+            <img
+              src="trc.png"
+              alt="TRC Logo"
+              className="w-42 h-20 object-contain md:w-40 md:h-20"
+            />
+          </div> */}
+        </div>
+      </div>
+      <div className="max-w-7xl mb-20 mt-20 mx-auto relative">
         {/* Section Title */}
         <h2
           className="text-center text-3xl md:text-4xl font-bold text-primary dark:text-primary mb-2 uppercase"
           data-aos="fade-down"
         >
-          Our Services
+          Our <span className="dark:text-white text-gray-700">Services</span> 
         </h2>
         <p
           className="text-center text-lg dark:text-white mb-8"
           data-aos="fade-up"
           data-aos-delay="100"
         >
-          From minor fixes to major repairs, we’ve got you covered.
+          From minor fixes to major repairs, <br /> we’ve got you covered.
         </p>
 
         <div className="relative overflow-hidden">
@@ -131,7 +137,7 @@ const ServiceSection = () => {
             {services.map((service, index) => (
               <div
                 key={index}
-                className="relative min-w-[300px] md:min-w-[400px] h-[420px] rounded-lg shadow-lg border-primary border
+                className="relative min-w-[300px] md:min-w-[400px] h-[520px] rounded-lg shadow-lg border-primary border
              hover:shadow-primary hover:border-primary/80 overflow-hidden group flex-shrink-0 border-primary hover:border-primary/80
              hover:scale-105 active:scale-105 bg-white hover:shadow-primary border-primary hover:border-primary/80 dark:bg-black/90 transition-all duration-300"
                 data-aos="fade-up"
@@ -142,7 +148,7 @@ const ServiceSection = () => {
                   <img
                     src={service.image}
                     alt={service.title}
-                    className="absolute inset-0 w-[95%] mt-15 h-55 object-cover"
+                    className="absolute inset-0 w-[90%] mx-auto mt-5 h-55 object-cover"
                     onError={(e) => {
                       e.currentTarget.onerror = null;
                       e.currentTarget.src = "/logo.jpg"; // fallback logo
@@ -166,8 +172,8 @@ const ServiceSection = () => {
                       bg-overlay dark:bg-overlay/30 hover:bg-primary dark:hover:bg-primary
                       text-primary dark:text-primary hover:text-dark transition-all duration-300"
                     >
-                      <FaArrowRight className="inline mr-2" />
-                      EXPLORE
+                      <FaArrowRight className="inline mr-2 transition-transform duration-300 group-hover:-translate-x-1" />
+                      <span className="transition-transform duration-300 group-hover:translate-x-2">EXPLORE</span>
                     </button>
                   </Link>
                 </div>
